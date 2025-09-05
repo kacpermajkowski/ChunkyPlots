971df776eda96a0455c59717cd4084847091bb31
Kacper Majkowski <kacpermajkowski@proton.me>
2025-09-05 10:20:46 +0200

    refactor(PlayerUtil): removed unused method

--- src/main/java/pl/kacpermajkowski/ChunkyPlots/utils/PlayerUtil.java
+++ src/main/java/pl/kacpermajkowski/ChunkyPlots/utils/PlayerUtil.java
@@ -13,43 +13,25 @@ import java.util.List;
 public class PlayerUtil {
 
     /**
-     * Returns an OfflinePlayer object if the player has previously joined the server
+     * Returns an OfflinePlayer object if the player has previously joined the server or null
      *
      * @param playerName name of a player that has previously joined the sever
      * @return OfflinePlayer object
      * @see OfflinePlayer
      * **/
-
     @Nullable
-    public static OfflinePlayer getOfflinePlayer(String playerName) {
-        for(OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
-            String offlinePlayerName = offlinePlayer.getName();
+    public static OfflinePlayer getOfflinePlayer(final String playerName) {
+        for(final OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
+            final String offlinePlayerName = offlinePlayer.getName();
             if(offlinePlayerName == null){
-                throw new IllegalStateException("OfflinePlayer.getName() from Bukkit.getOfflinePlayer() is null. This is most likely a Bukkit issue.");
+                throw new IllegalStateException(
+                        "OfflinePlayer.getName() from Bukkit.getOfflinePlayer() is null. " +
+                        "This is most likely a Bukkit issue. Try a newer/an older server build.");
             }
             if(offlinePlayerName.equalsIgnoreCase(playerName)) {
                 return offlinePlayer;
             }
         }
         return null;
-    }
-
-    public static Player findNearestPlayer(Location loc, int radius){
-        if(loc == null) return null;
-        Collection<Entity> nearbyEntities = loc.getWorld().getNearbyEntities(loc, radius, radius, radius);
-
-        Player closestPlayer = null;
-        for (Entity entity : nearbyEntities) {
-            if (!(entity instanceof Player player)) continue;
-
-            if (closestPlayer == null) closestPlayer = player;
-            else {
-                double closestPlayerDistance = closestPlayer.getLocation().distance(loc);
-                double playerDistance = player.getLocation().distance(loc);
-                closestPlayer = closestPlayerDistance < playerDistance ? closestPlayer : player;
-            }
-        }
-
-        return closestPlayer;
     }
 }
