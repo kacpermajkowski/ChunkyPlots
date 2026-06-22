175af40fce94a6ea031dfc0fc6953cd915ea37e7
Kacper Majkowski <kacpermajkowski@proton.me>
2026-06-22 08:43:26 +0200

    feat: PistonProtection relies on ProtectionUtil now

--- src/main/java/pl/kacpermajkowski/ChunkyPlots/protections/redstone/PistonProtection.java
+++ src/main/java/pl/kacpermajkowski/ChunkyPlots/protections/redstone/PistonProtection.java
@@ -1,7 +1,9 @@
 package pl.kacpermajkowski.ChunkyPlots.protections.redstone;
 
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -9,85 +11,62 @@ import org.bukkit.event.block.BlockPistonExtendEvent;
 import org.bukkit.event.block.BlockPistonRetractEvent;
 import pl.kacpermajkowski.ChunkyPlots.plot.Plot;
 import pl.kacpermajkowski.ChunkyPlots.plot.PlotManager;
+import pl.kacpermajkowski.ChunkyPlots.protections.ProtectionUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-
 
 public class PistonProtection implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onBlockPistonExtend(final BlockPistonExtendEvent event){
-        if(!canPistonAffectBlocks(event.getBlock(), event.getDirection(), event.getBlocks())){
+    public void onBlockPistonExtend(final BlockPistonExtendEvent event) {
+        if (!canPistonAffectBlocks(event.getBlock(), event.getDirection(), event.getBlocks())) {
             event.setCancelled(true);
         }
     }
+
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onBlockPistonRetract(final BlockPistonRetractEvent event){
-        if(!canPistonAffectBlocks(event.getBlock(), event.getDirection(), event.getBlocks())){
+    public void onBlockPistonRetract(final BlockPistonRetractEvent event) {
+        if (!canPistonAffectBlocks(event.getBlock(), event.getDirection(), event.getBlocks())) {
             event.setCancelled(true);
         }
     }
 
     private boolean canPistonAffectBlocks(Block piston, BlockFace direction, List<Block> blocks) {
         PlotManager plotManager = PlotManager.getInstance();
-
         Plot pistonPlot = plotManager.getPlot(piston);
+
+        Player pistonOwner = null;
+        if (pistonPlot != null) {
+            pistonOwner = Bukkit.getPlayer(pistonPlot.getOwnerUUID());
+        }
+
         Plot pistonHeadPlot = plotManager.getPlot(piston.getRelative(direction));
         List<Plot> blockPlots = getPlotsOfAllBlocks(blocks);
         List<Plot> affectedPlots = getPlotsAffectedByPiston(direction, blocks);
 
-        if(pistonPlot == null){
-            if(pistonHeadPlot != null)
-                return false;
-            else if (blockPlots.isEmpty())
-                return affectedPlots.isEmpty();
-            else
-                return false;
-        } else {
-            if(!pistonPlot.hasTheSameOwnerAs(pistonHeadPlot) && pistonHeadPlot != null)
-                return false;
-            else if(doPlotsMatchTheOwnerOrHaveNoOwner(affectedPlots, pistonPlot.getOwnerUUID()))
-                return doPlotsMatchTheOwnerOrHaveNoOwner(blockPlots, pistonPlot.getOwnerUUID());
-            else
-                return false;
-        }
-    }
+        if (!ProtectionUtil.canPlayerAffect(pistonOwner, pistonHeadPlot)) return false;
+        if (!ProtectionUtil.canPlayerAffectPlots(pistonOwner, affectedPlots)) return false;
+        if (!ProtectionUtil.canPlayerAffectPlots(pistonOwner, blockPlots)) return false;
 
-    private boolean doPlotsMatchTheOwnerOrHaveNoOwner(List<Plot> affectedPlots, UUID ownerUUID) {
-        for(Plot p : affectedPlots){
-            if(p != null) {
-                if (!p.isPlayerOwner(ownerUUID)) {
-                    return false;
-                }
-            }
-        }
         return true;
     }
 
     private List<Plot> getPlotsOfAllBlocks(List<Block> blocks) {
-        ArrayList<Plot> plots = new ArrayList<>();
-        for(Block b: blocks){
-            PlotManager plotManager = PlotManager.getInstance();
-
-            Plot plot = plotManager.getPlot(b.getChunk());
-
-            if(!plots.contains(plot) && plot != null)
+        List<Plot> plots = new ArrayList<>();
+        for (Block b : blocks) {
+            Plot plot = PlotManager.getInstance().getPlot(b.getChunk());
+            if (plot != null && !plots.contains(plot))
                 plots.add(plot);
         }
         return plots;
     }
 
     private List<Plot> getPlotsAffectedByPiston(BlockFace direction, List<Block> blocks) {
-        ArrayList<Plot> plots = new ArrayList<>();
-        for(Block b: blocks){
-            PlotManager plotManager = PlotManager.getInstance();
-
-            Block affectedBlock = b.getRelative(direction);
-            Plot affectedPlot = plotManager.getPlot(affectedBlock.getChunk());
-
-            if(!plots.contains(affectedPlot) && affectedPlot != null)
+        List<Plot> plots = new ArrayList<>();
+        for (Block b : blocks) {
+            Plot affectedPlot = PlotManager.getInstance().getPlot(b.getRelative(direction).getChunk());
+            if (affectedPlot != null && !plots.contains(affectedPlot))
                 plots.add(affectedPlot);
         }
         return plots;
